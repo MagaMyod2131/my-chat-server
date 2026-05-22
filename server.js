3f2c3995c7e23260d340543c2d94f42b45816744
MagaMyod2131 <Magomed20012025@outlook.com>
2026-05-22 13:43:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ function dmKey(a, b) { return [a, b].sort().join('::'); }
 function broadcast(room, data, exclude) {
   if (!rooms[room]) return;
   var json = JSON.stringify(data);
-  rooms[room].forEach(function(c) { if (c !== exclude && c.readyState === 1) c.send(json); });
+  rooms[room].forEach(function(c) {
+    if (c !== exclude && c.readyState === 1) c.send(json);
+  });
 }
 
 function broadcastAll(room, data) {
@@ -56,6 +58,7 @@ server.on('connection', function(ws) {
     try { msg = JSON.parse(raw); } catch(e) { return; }
 
     switch (msg.type) {
+
       case 'register':
         ws.userId = msg.userId;
         ws.name = msg.name;
@@ -65,12 +68,15 @@ server.on('connection', function(ws) {
 
       case 'find_user':
         var found = null;
-        if (msg.query && users[msg.query] && msg.query !== ws.userId) {
-          found = { userId: msg.query, name: userNames[msg.query], online: true };
+        // По ID
+        if (msg.query && msg.query !== ws.userId && userNames[msg.query]) {
+          found = { userId: msg.query, name: userNames[msg.query], online: !!users[msg.query] };
         }
+        // По имени
         if (!found) {
           for (var uid in userNames) {
-            if (uid !== ws.userId && userNames[uid] && userNames[uid].toLowerCase() === (msg.query||'').toLowerCase()) {
+            if (uid !== ws.userId && userNames[uid] &&
+                userNames[uid].toLowerCase() === (msg.query || '').toLowerCase()) {
               found = { userId: uid, name: userNames[uid], online: !!users[uid] };
               break;
             }
@@ -79,44 +85,60 @@ server.on('connection', function(ws) {
         ws.send(JSON.stringify({ type: 'find_result', query: msg.query, user: found }));
         break;
 
+      // ── ЛИЧНЫЕ СООБЩЕНИЯ ──
       case 'dm':
-        var k1 = dmKey(ws.userId, msg.toId);
-        var dm = { type: 'dm', fromId: ws.userId, toId: msg.toId, name: ws.name, text: msg.text, time: msg.time, id: msg.id, replyTo: msg.replyTo || null };
-        saveDM(k1, dm);
+        var k = dmKey(ws.userId, msg.toId);
+        var dm = {
+          type: 'dm', fromId: ws.userId, toId: msg.toId,
+          name: ws.name, text: msg.text, time: msg.time,
+          id: msg.id, replyTo: msg.replyTo || null
+        };
+        saveDM(k, dm);
         ws.send(JSON.stringify(dm));
         sendTo(msg.toId, dm);
         break;
 
       case 'dm_file':
-        var k2 = dmKey(ws.userId, msg.toId);
-        var dmf = { type: 'dm_file', fromId: ws.userId, toId: msg.toId, name: ws.name, fileType: msg.fileType, fileName: msg.fileName, fileSize: msg.fileSize, data: msg.data, time: msg.time, id: msg.id };
-        saveDM(k2, dmf);
+        var kf = dmKey(ws.userId, msg.toId);
+        var dmf = {
+          type: 'dm_file', fromId: ws.userId, toId: msg.toId,
+          name: ws.name, fileType: msg.fileType, fileName: msg.fileName,
+          fileSize: msg.fileSize, data: msg.data, time: msg.time, id: msg.id
+        };
+        saveDM(kf, dmf);
         ws.send(JSON.stringify(dmf));
         sendTo(msg.toId, dmf);
         break;
 
       case 'dm_edit':
-        var k3 = dmKey(ws.userId, msg.toId);
-        if (dmHistory[k3]) {
-          for (var i = 0; i < dmHistory[k3].length; i++) {
-            if (dmHistory[k3][i].id === msg.id && dmHistory[k3][i].fromId === ws.userId) {
-              dmHistory[k3][i].text = msg.text; dmHistory[k3][i].edited = true; break;
+        var ke = dmKey(ws.userId, msg.toId);
+        if (dmHistory[ke]) {
+          for (var i = 0; i < dmHistory[ke].length; i++) {
+            if (dmHistory[ke][i].id === msg.id && dmHistory[ke][i].fromId === ws.userId) {
+              dmHistory[ke][i].text = msg.text;
+              dmHistory[ke][i].edited = true;
+              break;
             }
           }
         }
         var ep = { type: 'dm_edit', fromId: ws.userId, toId: msg.toId, id: msg.id, text: msg.text };
-        ws.send(JSON.stringify(ep)); sendTo(msg.toId, ep);
+        ws.send(JSON.stringify(ep));
+        sendTo(msg.toId, ep);
         break;
 
       case 'dm_delete':
-        var k4 = dmKey(ws.userId, msg.toId);
-        if (dmHistory[k4]) {
-          for (var j = 0; j < dmHistory[k4].length; j++) {
-            if (dmHistory[k4][j].id === msg.id && dmHistory[k4][j].fromId === ws.userId) { dmHistory[k4].splice(j,1); break; }
+        var kd = dmKey(ws.userId, msg.toId);
+        if (dmHistory[kd]) {
+          for (var j = 0; j < dmHistory[kd].length; j++) {
+            if (dmHistory[kd][j].id === msg.id && dmHistory[kd][j].fromId === ws.userId) {
+              dmHistory[kd].splice(j, 1);
+              break;
+            }
           }
         }
         var dp = { type: 'dm_delete', fromId: ws.userId, toId: msg.toId, id: msg.id };
-        ws.send(JSON.stringify(dp)); sendTo(msg.toId, dp);
+        ws.send(JSON.stringify(dp));
+        sendTo(msg.toId, dp);
         break;
 
       case 'dm_history':
@@ -128,15 +150,23 @@ server.on('connection', function(ws) {
         var ck = dmKey(ws.userId, msg.toId);
         dmHistory[ck] = [];
         var cp = { type: 'dm_clear', fromId: ws.userId, toId: msg.toId };
-        ws.send(JSON.stringify(cp)); sendTo(msg.toId, cp);
+        ws.send(JSON.stringify(cp));
+        sendTo(msg.toId, cp);
         break;
 
       case 'dm_typing':
-        sendTo(msg.toId, { type: 'dm_typing', fromId: ws.userId, name: ws.name, isTyping: msg.isTyping, isSending: msg.isSending || false });
+        sendTo(msg.toId, {
+          type: 'dm_typing', fromId: ws.userId, name: ws.name,
+          isTyping: msg.isTyping, isSending: msg.isSending || false
+        });
         break;
 
+      // ── ЗВОНКИ P2P ──
       case 'call_offer':
-        sendTo(msg.toId, { type: 'call_offer', fromId: ws.userId, fromName: ws.name, sdp: msg.sdp, callType: msg.callType || 'audio' });
+        sendTo(msg.toId, {
+          type: 'call_offer', fromId: ws.userId, fromName: ws.name,
+          sdp: msg.sdp, callType: msg.callType || 'audio'
+        });
         break;
       case 'call_answer':
         sendTo(msg.toId, { type: 'call_answer', fromId: ws.userId, sdp: msg.sdp });
@@ -150,11 +180,22 @@ server.on('connection', function(ws) {
       case 'call_end':
         sendTo(msg.toId, { type: 'call_end', fromId: ws.userId });
         break;
+
+      // ── ГРУППОВЫЕ ЗВОНКИ ──
+      // Участник заходит в групповой звонок — сервер рассылает всем в комнате
       case 'group_call_join':
-        broadcast(msg.room, { type: 'group_call_join', fromId: ws.userId, fromName: ws.name }, ws);
+        if (!ws.room) break;
+        broadcast(ws.room, {
+          type: 'group_call_join', fromId: ws.userId, fromName: ws.name
+        }, ws);
         break;
+
+      // Один участник шлёт offer конкретному другому (mesh-топология)
       case 'group_call_offer':
-        sendTo(msg.toId, { type: 'group_call_offer', fromId: ws.userId, fromName: ws.name, sdp: msg.sdp, callType: msg.callType || 'audio' });
+        sendTo(msg.toId, {
+          type: 'group_call_offer', fromId: ws.userId, fromName: ws.name,
+          sdp: msg.sdp, callType: msg.callType || 'audio'
+        });
         break;
       case 'group_call_answer':
         sendTo(msg.toId, { type: 'group_call_answer', fromId: ws.userId, sdp: msg.sdp });
@@ -163,36 +204,66 @@ server.on('connection', function(ws) {
         sendTo(msg.toId, { type: 'group_call_ice', fromId: ws.userId, candidate: msg.candidate });
         break;
       case 'group_call_leave':
-        broadcast(msg.room, { type: 'group_call_leave', fromId: ws.userId, fromName: ws.name }, ws);
+        if (!ws.room) break;
+        broadcast(ws.room, {
+          type: 'group_call_leave', fromId: ws.userId, fromName: ws.name
+        }, ws);
         break;
 
+      // ── ГРУППЫ ──
       case 'join':
         if (ws.room && rooms[ws.room]) {
           rooms[ws.room].delete(ws);
-          broadcast(ws.room, { type: 'system', room: ws.room, text: ws.name + ' вышел из комнаты', members: getMembers(ws.room) });
+          broadcast(ws.room, {
+            type: 'system', room: ws.room,
+            text: (ws.name || '?') + ' вышел из комнаты',
+            members: getMembers(ws.room)
+          });
         }
-        ws.room = msg.room; ws.name = msg.name;
+        ws.room = msg.room;
+        ws.name = msg.name;
         if (!rooms[msg.room]) rooms[msg.room] = new Set();
         rooms[msg.room].add(ws);
-        broadcast(msg.room, { type: 'system', room: msg.room, text: msg.name + ' подключился', members: getMembers(msg.room) }, ws);
-        ws.send(JSON.stringify({ type: 'init', room: msg.room, members: getMembers(msg.room), history: history[msg.room] || [] }));
+        broadcast(msg.room, {
+          type: 'system', room: msg.room,
+          text: msg.name + ' подключился',
+          members: getMembers(msg.room)
+        }, ws);
+        ws.send(JSON.stringify({
+          type: 'init', room: msg.room,
+          members: getMembers(msg.room),
+          history: history[msg.room] || []
+        }));
         break;
 
       case 'message':
-        var gm = { type: 'message', room: ws.room, name: ws.name, text: msg.text, time: msg.time, replyTo: msg.replyTo || null, id: msg.id };
-        saveMsg(ws.room, gm); broadcastAll(ws.room, gm);
+        var gm = {
+          type: 'message', room: ws.room, name: ws.name,
+          text: msg.text, time: msg.time,
+          replyTo: msg.replyTo || null, id: msg.id
+        };
+        saveMsg(ws.room, gm);
+        broadcastAll(ws.room, gm);
         break;
 
       case 'file':
-        var gf = { type: 'file', room: ws.room, name: ws.name, fileType: msg.fileType, fileName: msg.fileName, fileSize: msg.fileSize, data: msg.data, time: msg.time, id: msg.id, replyTo: msg.replyTo || null };
-        saveMsg(ws.room, gf); broadcastAll(ws.room, gf);
+        var gf = {
+          type: 'file', room: ws.room, name: ws.name,
+          fileType: msg.fileType, fileName: msg.fileName,
+          fileSize: msg.fileSize, data: msg.data,
+          time: msg.time, id: msg.id, replyTo: msg.replyTo || null
+        };
+        saveMsg(ws.room, gf);
+        broadcastAll(ws.room, gf);
         break;
 
       case 'edit':
         if (history[ws.room]) {
           for (var ei = 0; ei < history[ws.room].length; ei++) {
             if (history[ws.room][ei].id === msg.id && history[ws.room][ei].name === ws.name) {
-              history[ws.room][ei].text = msg.text; history[ws.room][ei].edited = true; break;
+              history[ws.room][ei].text = msg.text;
+              history[ws.room][ei].edited = true;
+              break;
             }
           }
         }
@@ -202,7 +273,10 @@ server.on('connection', function(ws) {
       case 'delete':
         if (history[ws.room]) {
           for (var di = 0; di < history[ws.room].length; di++) {
-            if (history[ws.room][di].id === msg.id && history[ws.room][di].name === ws.name) { history[ws.room].splice(di,1); break; }
+            if (history[ws.room][di].id === msg.id && history[ws.room][di].name === ws.name) {
+              history[ws.room].splice(di, 1);
+              break;
+            }
           }
         }
         broadcastAll(ws.room, { type: 'delete', room: ws.room, id: msg.id, name: ws.name });
@@ -214,7 +288,10 @@ server.on('connection', function(ws) {
         break;
 
       case 'typing':
-        broadcast(ws.room, { type: 'typing', room: ws.room, name: ws.name, isTyping: msg.isTyping, isSending: msg.isSending || false }, ws);
+        broadcast(ws.room, {
+          type: 'typing', room: ws.room, name: ws.name,
+          isTyping: msg.isTyping, isSending: msg.isSending || false
+        }, ws);
         break;
     }
   });
@@ -223,7 +300,11 @@ server.on('connection', function(ws) {
     if (ws.userId) delete users[ws.userId];
     if (ws.room && rooms[ws.room]) {
       rooms[ws.room].delete(ws);
-      broadcast(ws.room, { type: 'system', room: ws.room, text: (ws.name||'?') + ' вышел из комнаты', members: getMembers(ws.room) });
+      broadcast(ws.room, {
+        type: 'system', room: ws.room,
+        text: (ws.name || '?') + ' вышел из комнаты',
+        members: getMembers(ws.room)
+      });
     }
   });
 
@@ -234,4 +315,5 @@ server.on('connection', function(ws) {
 });
 
 console.log('Сервер запущен!');
+
 
